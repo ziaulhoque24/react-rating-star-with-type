f9417b9aca9ab6d27e17b04943d1d7875ae86101
Ziaul Hoque <76832971+ziaulhoque24@users.noreply.github.com>
2023-06-17 07:51:53 +0600

    delete production build

--- dist/icons.d.ts
+++
@@ -1,5 +0,0 @@
-import * as React from "react";
-export declare function BsStar(props: any): React.JSX.Element;
-export declare function BsStarFill(props: any): React.JSX.Element;
-export declare function BsStarHalf(props: any): React.JSX.Element;
-//# sourceMappingURL=icons.d.ts.map
